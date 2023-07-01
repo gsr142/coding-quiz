918484ec09a47659fc40171f5bc7cc263ea6a069
gsr142 <gsr142@gmail.com>
2023-07-01 13:54:02 -0700

    add elements in JS. they probably dont work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
+var body = document.body;
+var button = document.createElement("button");
+button.setAttribute("style", "width: 40px; font-size: 16px; background: blue");
+
 var clock = document.querySelector(".clock");
 var quiz = document.querySelector(".quiz");
-
 var timeLeft = 10;
+clock.setAttribute("style", "width: 50px; height: 20px");
+body.quiz.appendChild(button);
 
-
-function quizTimer () {
+//setTime runs timer element inside clock class. Currently functional. Don't change it.
+function setTime() {
+    
     var timerInterval = setInterval(function() {
-        timeLeft--;
-        clock.textContent = "Time remaining: " + timeLeft + " seconds";
-
-        if (timeLeft === 0) {
-            clearInterval(timerInterval);
-            //add function to end quiz here
-
-        
-        }
+      timeLeft--; //decrements time left by 1, every 1000ms
+      clock.textContent = "You have " + timeLeft + " seconds remaining"; //displays time remaining 
+      if(timeLeft === 0) {
+        clearInterval(timerInterval);
+      } 
     }, 1000);
-}
-quizTimer();
+  }
+clock.appendChild(setTime());
+
+
