ff53f0aaa1e2e911ab3692c5ec7c3328d7a693d0
gsr142 <gsr142@gmail.com>
2023-07-02 18:59:59 -0700

    add welcome and start quiz button, tied button to timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 var body = document.body;
-var button = document.createElement("button");
-button.setAttribute("style", "width: 40px; font-size: 16px; background: blue");
+//Create answer buttons
+var startButton = document.createElement("button");
+startButton.setAttribute("style", "font-size: 16px; background: blue; color: white");
+startButton.textContent = "Begin quiz";
+
+var button2 = document.createElement("button");
+button2.setAttribute("style", "font-size: 16px; background: blue; color: white");
+
+var button3 = document.createElement("button");
+button3.setAttribute("style", "font-size: 16px; background: blue; color: white");
+
+var button4 = document.createElement("button");
+button4.setAttribute("style", "font-size: 16px; background: blue; color: white");
 
 var clock = document.querySelector(".clock");
 var quiz = document.querySelector(".quiz");
 var timeLeft = 10;
-clock.setAttribute("style", "width: 50px; height: 20px");
-body.quiz.appendChild(button);
+//clock.setAttribute("style", "width: 50px; height: 20px");
+var dummy = document.createElement("div");
 
+quiz.appendChild(startButton);
+startButton.addEventListener("click", setTime)
 //setTime runs timer element inside clock class. Currently functional. Don't change it.
 function setTime() {
     
@@ -19,6 +32,6 @@ function setTime() {
       } 
     }, 1000);
   }
-clock.appendChild(setTime());
+
 
 
